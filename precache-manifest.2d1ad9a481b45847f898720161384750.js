856fe3129d976cbaac657daeb66c3b9407414f2e
Pushpit <pushpit07@gmail.com>
2021-05-30 16:29:47 +0530

    Updates

---
+++ precache-manifest.2d1ad9a481b45847f898720161384750.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c1b8053bc2974daadac",
+    "url": "/HashPuppies/static/css/main.7672cdab.chunk.css"
+  },
+  {
+    "revision": "5c1b8053bc2974daadac",
+    "url": "/HashPuppies/static/js/main.5c1b8053.chunk.js"
+  },
+  {
+    "revision": "4597834e10fbd5ec70f6",
+    "url": "/HashPuppies/static/css/1.2a400d36.chunk.css"
+  },
+  {
+    "revision": "4597834e10fbd5ec70f6",
+    "url": "/HashPuppies/static/js/1.4597834e.chunk.js"
+  },
+  {
+    "revision": "bb5c5dab6b2154358f47",
+    "url": "/HashPuppies/static/js/runtime~main.bb5c5dab.js"
+  },
+  {
+    "revision": "acc4685523a7d5fca33379dd0d5d6d82",
+    "url": "/HashPuppies/index.html"
+  }
+];
